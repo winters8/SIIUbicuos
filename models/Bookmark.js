c4da2f846e43f094a70ad14612efb1544f0fa5e3
Ivan Gonzalez Diaz <ivan.gdiaz@uclm.es>
2022-10-11 12:41:09 +0200

    MongoDB Atlas connection added

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+// var Movie = require('../models/Movie.js'); // necesario si no estamos dentro del mismo directorio
+
 var BookmarkSchema = new Schema({
     email: String,
     movie: { type: Schema.ObjectId, ref: 'Movie' },
