19139a271ae5f2c9b9ac857bd1bdb952078b4c57
Ivan Gonzalez Diaz <ivan.gdiaz@uclm.es>
2022-10-11 12:00:14 +0200

    models added 2...

--- models/Movie.js
+++ models/Movie.js
@@ -1,15 +1,14 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var MovieSchema = new Schema({
-country: String,
-director: String,
-category: [String],
-plot: String,
-poster: String,
-year: Number,
-title: String,
-trailer: String,
-imdbRating: String,
-id: Number
-});
-module.exports = mongoose.model('Movie', MovieSchema);
+  country: String,
+  director: String,
+  category: [String],
+  plot: String,
+  poster: String,
+  year: Number,
+  title: String,
+  trailer: String,
+  imdbRating: Number
+}); // El ObjectId está implícito
+module.exports = mongoose.model("Movie", MovieSchema); // define el modelo
